86ec3f40ca2b2ba6312ce8ec0fff1aa1c9d088d3
Sandeep Suryavanhi <sandeep.suryavanshi09@yahoo.com>
2016-08-16 14:12:13 +0530

    This is My Solution

---
+++ GitHubSample/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GitHubSample
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
